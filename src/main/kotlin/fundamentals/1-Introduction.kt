50ce16fb89231f37035214726ad7d433e43a68d2
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-01-21 17:59:25 -0500

    Kotlin Introduction

---
+++ src/main/kotlin/fundamentals/1-Introduction.kt
@@ -0,0 +1,19 @@
+package fundamentals
+
+/**
+ *  WHAT is Kotlin ?
+ *  1. Kotlin is a modern programming language by JetBrains. (A statically programmed language i.e. recognises most of the errors on compile time)
+ *  2. It's a cross-platform language & can be used for Android development, web-frontend, server-side or Multiplatform.
+ *  3. Kotlin code runs on JVM is eventually converted to byte code.
+ *  4. Open Source
+ *
+ *  WHY Kotlin ?
+ *  1. Makes code small, concise, readable & maintainable
+ *  2. Null Safe and removes a lot of boilerplate code
+ *  3. Object-oriented(classes, objects, encapsulation ets..) & functional(higher order functions, lambdas, first class functions etc.)
+ *
+ */
+
+fun main(){
+    println("Welcome to Kotlin Programming")
+}
