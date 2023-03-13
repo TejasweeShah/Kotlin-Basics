01c729bc1f902392bbd312527b55f0e9514566f6
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-03-13 17:24:06 -0400

    Visibility Modifiers - Inside Package

---
+++ src/main/kotlin/object_oriented_programming/4 - Visibility Modifiers - Inside Package.kt
@@ -0,0 +1,20 @@
+package object_oriented_programming
+
+/**
+ * ## Visibility Modifiers
+ *
+ * ### Visibility Modifiers Inside Package
+ *
+ * public : DEFAULT, Declarations are visible everywhere
+ *
+ * private :    Visible inside the file containing the declaration
+ *
+ * internal :   Visible inside the same module (a set of Kotlin files compiled together)
+ *
+ * protected :  Not available for packages (used for subclasses)
+ *
+ */
+
+fun main() {
+
+}
