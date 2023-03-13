67636face775957c41e5f58bcf642d7be210b225
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-03-13 17:22:26 -0400

    Object-Oriented Concepts in Kotlin

---
+++ src/main/kotlin/object_oriented_programming/1 - Introduction.kt
@@ -0,0 +1,17 @@
+package object_oriented_programming
+
+/**
+ * ## Kotlin Object Oriented Programming
+ * 1. Classes :::
+ * By default all the classes in Kotlin are final(so we cannot inherit them). To inherit  a class we need to put open keyword in front of a class.
+ *
+ * * Inheritance in Kotlin
+ * * Encapsulation
+ * * Interfaces in Kotlin
+ * * Visibility Modifiers
+ * * Extensions in Kotlin
+ */
+
+fun main() {
+
+}
